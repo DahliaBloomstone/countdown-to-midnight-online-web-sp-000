0fcdb6bc3bfc8ed8f76510bc93c0649cb233638f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 01:06:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,24 +1,7 @@
-def countdown(number)
-while seconds > 0
-  puts "#{seconds} SECOND(S)!"
-  SECONDS -= 1
-end
-return "HAPPY NEW YEAR!"
-end
-
 def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
